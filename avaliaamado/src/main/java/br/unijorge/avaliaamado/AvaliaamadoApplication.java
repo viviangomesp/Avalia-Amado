9d726572781ddd4337de1fee2d43e1a6ed1cbd97
Vivian Gomes <viviangomesp@hotmail.com>
2025-05-21 16:43:49 -0300

    Create Spring Project

---
+++ avaliaamado/src/main/java/br/unijorge/avaliaamado/AvaliaamadoApplication.java
@@ -0,0 +1,13 @@
+package br.unijorge.avaliaamado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliaamadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliaamadoApplication.class, args);
+	}
+
+}
---
+++ avaliaamado/src/test/java/br/unijorge/avaliaamado/AvaliaamadoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.unijorge.avaliaamado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliaamadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
